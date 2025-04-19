068151d47ea5f63a39104f00be2cd7c2345b4eb7
Salmon-0103 <absolute.still@gmail.com>
2025-04-20 01:41:22 +0800

    Fix(login): 把login整合到login資料夾裡 Feat(login): 增加查看所有貼文按紐

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,32 +4,32 @@ import Link from 'next/link';
 import styles from '@/styles/Home.module.css';
 
 export default function Home() {
-  const [posts, setPosts] = useState([]);
+	const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://localhost:3001/api/posts')
-      .then(res => setPosts(res.data))
-      .catch(err => console.error('取得貼文失敗:', err));
-  }, []);
+	useEffect(() => {
+		axios
+			.get('http://localhost:3001/api/posts')
+			.then((res) => setPosts(res.data))
+			.catch((err) => console.error('取得貼文失敗:', err));
+	}, []);
 
-  return (
-    <div className="container py-4">
-      <h1 className="mb-4"> KnockKnock 貼文區</h1>
-      {posts.map(post => (
-        <div className="card mb-3" key={post.id}>
-          <Link href={`/posts/${post.id}`}>
-            查看留言
-          </Link>
-          <div className="card-body">
-            <h5 className="card-title">{post.title}</h5>
-            <p className="card-text">{post.content}</p>
-            <p className="text-muted small">
-            👤by {post.author} ・🕒{new Date(post.createdAt).toLocaleString()}
-            </p>
-          </div>
-        </div>
-      ))}
-     
-    </div>
-  );
+	return (
+		<>
+			<div className="container py-4">
+				<h1 className="mb-4"> KnockKnock 貼文區</h1>
+				{posts.map((post) => (
+					<div className="card mb-3" key={post.id}>
+						<Link href={`/posts/${post.id}`}>查看留言</Link>
+						<div className="card-body">
+							<h5 className="card-title">{post.title}</h5>
+							<p className="card-text">{post.content}</p>
+							<p className="text-muted small">
+								👤by {post.author} ・🕒{new Date(post.createdAt).toLocaleString()}
+							</p>
+						</div>
+					</div>
+				))}
+			</div>
+		</>
+	);
 }
