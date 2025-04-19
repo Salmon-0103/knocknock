c7af2b72264c993f7bb80dc887696965381d2e50
Salmon-0103 <absolute.still@gmail.com>
2025-04-20 01:44:38 +0800

    Feat(register): 增加所有欄位未輸入時的提示內容，未來要做的相關功能註解 Fix(register): 修正輸入密碼後，格式正確提示還在

--- client/pages/login/register.js
+++ client/pages/login/register.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 
@@ -18,11 +18,6 @@ export default function RegisterPage() {
 
 	const handleRegister = async () => {
 		setErrMsg('');
-
-		if (password !== confirmPassword) {
-			return setErrMsg('兩次密碼不一致');
-		}
-
 		if (!validatePassword(password)) {
 			return setErrMsg('密碼需至少8字，包含小寫英文與數字');
 		}
@@ -53,56 +48,63 @@ export default function RegisterPage() {
 		}
 	};
 
+	useEffect(() => {
+		setPwdValid(validatePassword(password));
+		setPwdMatch(password === confirmPassword);
+	}, [password, confirmPassword]);
+
 	return (
 		<div className="container py-5" style={{ maxWidth: 400 }}>
-			<h2 className="mb-4">📝 註冊新帳號</h2>
+			<h1 className="mb-4">Sign in</h1>
 			{errMsg && <div className="alert alert-danger">{errMsg}</div>}
-
+			{/* 使用者名稱 */}
 			<div className="mb-3">
 				<label>使用者名稱</label>
 				<input
 					className="form-control"
+					placeholder="請輸入使用者名稱"
 					value={username}
 					onChange={(e) => setUsername(e.target.value)}
 				/>
 			</div>
+			{/* 電子信箱 */}
 			<div className="mb-3">
 				<label>Email</label>
 				<input
 					className="form-control"
+					placeholder="請輸入電子信箱"
 					value={email}
 					onChange={(e) => setEmail(e.target.value)}
 				/>
 			</div>
+			{/* 密碼 */}
 			<div className="mb-3">
 				<label>密碼</label>
 				<input
 					type="password"
 					className={`form-control ${password && !pwdValid ? 'is-invalid' : ''}`}
+					placeholder="請輸入密碼"
 					value={password}
 					onChange={(e) => setPassword(e.target.value)}
 				/>
 				{/* 密碼如果是空的 */}
 				{!password && <div className="form-text">請輸入密碼</div>}
 				{/* 密碼需符合規定 */}
+				{/* 加入眼睛圖示、做開關確認密碼功能 */}
 				{!pwdValid && password && (
 					<div className="invalid-feedback">
-						密碼格式不正確，需至少8字，且小寫英文與數字混合
+						密碼格式不正確(需至少8字，包含小寫英文、數字)
 					</div>
 				)}
 			</div>
-			<div className="mb-3">
-				<label>再次輸入密碼</label>
-				<input
-					type="password"
-					className="form-control"
-					value={confirmPassword}
-					onChange={(e) => setConfirmPassword(e.target.value)}
-				/>
-			</div>
-			<button className="btn btn-success w-100" onClick={handleRegister}>
+			{/* 生日 */}
+			<div className="mb-3"></div>
+			{/* checkbox同意使用條款 */}
+			{/* 註冊按紐 */}
+			<button className="btn btn-success w-100 " onClick={handleRegister}>
 				註冊
 			</button>
+			{/* 已經有帳號了了嗎?<LINK href="/login">登入</=> */}
 		</div>
 	);
 }
