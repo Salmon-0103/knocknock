6c221da1f363d2a16608819e2ac3c7fd725aa011
Salmon-0103 <absolute.still@gmail.com>
2025-04-20 01:49:15 +0800

    Feat(componets): 將抓取到的元件、會員狀態引入

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,12 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css';
+import { UserProvider } from '@/contexts/userContext';
+import Header from '@/components/header';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	return (
+		<UserProvider>
+			<Header />
+			<Component {...pageProps} />
+		</UserProvider>
+	);
 }
